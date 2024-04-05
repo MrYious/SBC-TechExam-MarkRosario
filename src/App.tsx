85e6a74d4ac2b598f45910dad0f54cbf0788b76b
MrYious <rosariomark37@gmail.com>
2024-04-05 16:57:32 +0800

    fetch data upon load of site then save to redux store

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,24 @@ import './App.scss'
 
 import { NavBar } from './components/NavBar';
 import { Outlet } from 'react-router-dom';
+import { loadRecipes } from './slicers/RecipeSlicer';
+import { useAppDispatch } from './hooks/useReduxHooks';
+import { useEffect } from 'react';
 
 function App() {
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    fetch('/api/data.json')
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res);
+      dispatch(loadRecipes(res));
+    })
+    .catch((err) => console.log(err));
+  }, [])
+
 
   return (
     <>
