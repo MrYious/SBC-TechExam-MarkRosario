a65486dd904fca95bccdea4f56371275c8784c72
MrYious <rosariomark37@gmail.com>
2024-04-06 07:26:14 +0800

    Remove comment

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -8,8 +8,6 @@ import { useEffect } from 'react';
 
 function Home() {
 
-  // TODO: Filter
-
   const recipes = useAppSelector((state) => state.recipes);
   const userPreference = useAppSelector((state) => state.userPreference);
 
