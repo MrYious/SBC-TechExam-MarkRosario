a24f4ff84f21c8c8840488403cafa36840be197d
MrYious <rosariomark37@gmail.com>
2024-04-05 19:12:50 +0800

    update RecipeItem to preview data

--- src/components/RecipeItem.tsx
+++ src/components/RecipeItem.tsx
@@ -1,12 +1,31 @@
 import { Recipe } from '../slicers/RecipeSlicer'
 import iconStarFilled from '../assets/star-filled.svg'
-import iconStarOutline from '../assets/star-outline.svg'
+import iconStarOutlined from '../assets/star-outline.svg'
 
 export const RecipeItem = (props: {recipe: Recipe}) => {
 
     return (
         <div className='recipeItem'>
-            {props.recipe.title}
+            <div className='imageHolder'>
+                <img src={props.recipe.image} alt="recipe image" id="cover" />
+                <button onClick={() => {}}>
+                    {
+                        false ?
+                            <img src={iconStarFilled} alt="icon star filled " className='icon' />
+                        :
+                            <img src={iconStarOutlined} alt="icon star outlined" className='icon'/>
+                    }
+                </button>
+            </div>
+            <div className='recipeDetails'>
+                <h1 className='recipeTitle'>Hello</h1>
+                <p className='recipeDesc'>{props.recipe.description}</p>
+                <p className='seeMore'>See More</p>
+                <div className='footer'>
+                    <p id='name'> Added by: {props.recipe.name}</p>
+                    <p id='dateAdded'>Date: {props.recipe.dateAdded}</p>
+                </div>
+            </div>
         </div>
     )
 }
