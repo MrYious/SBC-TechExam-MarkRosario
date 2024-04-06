c3ef393f1ba5cd061ec64fd2007b6cb5b9ac1267
MrYious <rosariomark37@gmail.com>
2024-04-06 19:35:34 +0800

    Refactor recipe pages to use the same style

--- src/pages/NewRecipe/NewRecipe.tsx
+++ src/pages/NewRecipe/NewRecipe.tsx
@@ -1,17 +1,34 @@
-import './NewRecipe.scss'
+import '../Recipe/Recipe.scss'
 
 import { Recipe, createNewRecipe } from '../../slicers/RecipeSlicer';
+import { useEffect, useState } from 'react';
 
 import { RecipePageForm } from '../../components/RecipePageForm';
+import { startNewRecipe } from '../../slicers/SelectRecipeSlicer';
 import { useAppDispatch } from '../../hooks/useReduxHooks';
 
 function NewRecipe() {
 
 	const dispatch = useAppDispatch();
+  const [loading, setLoading] = useState(true)
 
 	const handleSave = (recipe: Recipe) => {
 		dispatch(createNewRecipe(recipe));
 	}
+
+  useEffect(() => {
+    // Resets the state incase it contains state of accessed recipes
+    dispatch(startNewRecipe());
+    setLoading(false)
+  }, [])
+
+  if (loading) {
+    return (
+      <main id='recipe' className={`${loading && 'loading'}`}>
+        <h2>Loading the recipe...</h2>
+      </main>
+    )
+  }
 
   return (
     <main id='newRecipe'>
