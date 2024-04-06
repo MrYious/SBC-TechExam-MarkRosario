99f8a304553ff17fb5baa7e58f071145de19ef29
MrYious <rosariomark37@gmail.com>
2024-04-06 17:30:45 +0800

    Fix: form button

--- src/components/CustomFormButton.tsx
+++ src/components/CustomFormButton.tsx
@@ -2,7 +2,7 @@
 interface FormButton {
     text: string
     type: 'save' | 'delete'
-    action: () => void
+    action: () => void;
 }
 
 export const CustomFormButton = (props: FormButton) => {
@@ -10,6 +10,7 @@ export const CustomFormButton = (props: FormButton) => {
     return (
         <button
             className={`formButton ${props.type}`}
+            type={props.type === 'save' ? 'submit' : 'button'}
             onClick={props.action}
         >
             {props.text}
