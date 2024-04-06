06b6cfc4beab62ebed80e1861070763d9779f063
MrYious <rosariomark37@gmail.com>
2024-04-06 18:40:30 +0800

    Update router

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,6 @@
 import App from "./App";
 import Home from "./pages/Home/Home";
+import NewRecipe from "./pages/NewRecipe/NewRecipe";
 import Recipe from "./pages/Recipe/Recipe";
 import { createBrowserRouter } from "react-router-dom";
 
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
             {
                 path: "recipe/:title",
                 element: <Recipe />
+            },
+            {
+                path: "create",
+                element: <NewRecipe />
             }
         ]
     }
