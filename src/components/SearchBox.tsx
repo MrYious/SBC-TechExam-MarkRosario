31d4d2d07be912d2ffd1b9dab13e22b6dd31171f
MrYious <rosariomark37@gmail.com>
2024-04-06 22:18:45 +0800

    Add search string state to component and refactor

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,17 +1,26 @@
+import { useAppDispatch, useAppSelector } from '../hooks/useReduxHooks'
+
 import iconSearch from '../assets/search.svg'
+import { updateSearchString } from '../slicers/UserPreferenceSlicer';
 
 export const SearchBox = () => {
+
+    const searchString = useAppSelector(state => state.userPreference.searchString);
+    const dispatch = useAppDispatch();
+
+    const handleUpdateSearchString = (e: React.ChangeEvent<HTMLInputElement>) => {
+        dispatch(updateSearchString(e.target.value));
+    }
 
     return (
         <div className="searchBox">
             <input
                 type="text"
                 placeholder="Search here..."
-                required
+                value={searchString}
+                onChange={handleUpdateSearchString}
             />
-            <button onClick={()=>{}}>
-                <img src={iconSearch} alt="search icon" />
-            </button>
+            <img src={iconSearch} alt="search icon" />
         </div>
     )
 }
