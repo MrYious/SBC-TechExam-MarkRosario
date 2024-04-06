b8b971542b89946ea6bf722da92c05bc02475acf
MrYious <rosariomark37@gmail.com>
2024-04-06 18:45:38 +0800

    Add function to create button

--- src/components/NewRecipeButton.tsx
+++ src/components/NewRecipeButton.tsx
@@ -1,9 +1,16 @@
 import iconCross from '../assets/cross.svg'
+import { useNavigate } from 'react-router-dom'
 
 export const NewRecipeButton = () => {
 
+    const navigate = useNavigate();
+
+    const handleCreateNewRecipe = () => {
+        navigate('create');
+    }
+
     return (
-        <button onClick={()=>{}} className='newRecipeButton'>
+        <button onClick={handleCreateNewRecipe} className='newRecipeButton'>
             <img src={iconCross} alt="icon cross" />
         </button>
     )
