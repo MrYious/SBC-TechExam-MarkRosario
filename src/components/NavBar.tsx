54c8d3cba3030fd043fa00d59f3b196d789a851e
MrYious <rosariomark37@gmail.com>
2024-04-07 10:54:54 +0800

    Add dynamic page title

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,22 @@
+import { useLocation, useParams } from "react-router-dom"
+
 import { SearchBox } from "./SearchBox"
-import { useLocation } from "react-router-dom"
+import { useEffect } from "react";
 
 export const NavBar = () => {
     const location = useLocation();
+    const { title } = useParams();
+
+    useEffect(() => {
+        console.log();
+        if (location.pathname === '/') {
+            document.title = 'Home | Recipes';
+        } else if (location.pathname === '/create') {
+            document.title = 'Create Recipe'
+        } else {
+            document.title = 'Recipe | ' + title;
+        }
+    }, [location]);
 
     return (
         <nav>
